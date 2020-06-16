303b8185380ab23249975af52053ea63abbd3aac
Gökmen Görgen <gkmngrgn@gmail.com>
2020-06-16 10:51:13 +0200

    change camera position.

--- rayt-cpp/main.cpp
+++ rayt-cpp/main.cpp
@@ -62,7 +62,8 @@ int main() {
   world.add(make_shared<sphere>(point3(-1, 0, -1), -0.45,
                                 make_shared<dielectric>(1.5)));
 
-  camera cam;
+  vec3 vup(0, 1, 0);
+  camera cam(point3(-2, 2, 1), point3(0, 0, -1), vup, 20, aspect_ratio);
 
   for (int j = image_height - 1; j >= 0; --j) {
     std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
