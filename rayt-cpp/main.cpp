a5610e4a2b830ade082a1a719cb2e3cc19d27924
Gökmen Görgen <gkmngrgn@gmail.com>
2020-06-16 11:48:01 +0200

    random scene. defocus blur is still not working.

--- rayt-cpp/main.cpp
+++ rayt-cpp/main.cpp
@@ -41,6 +41,52 @@ color ray_color(const ray &r, const hittable &world, int depth) {
   return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
+hittable_list random_scene() {
+  hittable_list world;
+
+  auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+  world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
+
+  for (int a = -11; a < 11; a++) {
+    for (int b = -11; b < 11; b++) {
+      auto choose_mat = random_double();
+      point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
+
+      if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+        shared_ptr<material> sphere_material;
+
+        if (choose_mat < 0.8) {
+          // diffuse
+          auto albedo = color::random() * color::random();
+          sphere_material = make_shared<lambertian>(albedo);
+          world.add(make_shared<sphere>(center, 0.2, sphere_material));
+        } else if (choose_mat < 0.95) {
+          // metal
+          auto albedo = color::random(0.5, 1);
+          auto fuzz = random_double(0, 0.5);
+          sphere_material = make_shared<metal>(albedo, fuzz);
+          world.add(make_shared<sphere>(center, 0.2, sphere_material));
+        } else {
+          // glass
+          sphere_material = make_shared<dielectric>(1.5);
+          world.add(make_shared<sphere>(center, 0.2, sphere_material));
+        }
+      }
+    }
+  }
+
+  auto material1 = make_shared<dielectric>(1.5);
+  world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+
+  auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+  world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+
+  auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+  world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+
+  return world;
+}
+
 int main() {
   const auto aspect_ratio = 16.0 / 9.0;
   const int image_width = 384;
@@ -50,23 +96,13 @@ int main() {
 
   std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-  hittable_list world;
-  world.add(make_shared<sphere>(point3(0, 0, -1), 0.5,
-                                make_shared<lambertian>(color(0.1, 0.2, 0.5))));
-  world.add(make_shared<sphere>(point3(0, -100.5, -1), 100,
-                                make_shared<lambertian>(color(0.8, 0.8, 0.0))));
-  world.add(make_shared<sphere>(point3(1, 0, -1), 0.5,
-                                make_shared<metal>(color(0.8, 0.6, 0.2), 0.3)));
-  world.add(make_shared<sphere>(point3(-1, 0, -1), 0.5,
-                                make_shared<dielectric>(1.5)));
-  world.add(make_shared<sphere>(point3(-1, 0, -1), -0.45,
-                                make_shared<dielectric>(1.5)));
+  auto world = random_scene();
 
-  point3 lookfrom(3, 3, 2);
-  point3 lookat(0, 0, -1);
+  point3 lookfrom(13, 2, 3);
+  point3 lookat(0, 0, 0);
   vec3 vup(0, 1, 0);
-  auto dist_to_focus = (lookfrom - lookat).length();
-  auto aperture = 2.0;
+  auto dist_to_focus = 10.0;
+  auto aperture = 0.1;
 
   camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
 
