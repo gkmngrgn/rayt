60e18b8702d2a66274be4bc9f8cc9c6c4e150d13
Gökmen Görgen <gkmngrgn@gmail.com>
2020-07-05 00:28:10 +0200

    activate all materials in python.

--- rayt-python/rayt_python/main.py
+++ rayt-python/rayt_python/main.py
@@ -2,7 +2,6 @@ import asyncio
 import itertools
 import math
 import sys
-import typing
 
 from rayt_python.camera import Camera
 from rayt_python.color import get_color_str, write_color
@@ -51,27 +50,27 @@ def random_scene() -> HittableList:
     ground_material = Lambertian(albedo=Color(0.5, 0.5, 0.5))
     world.add(Sphere(Point3(0, -1000, 0), 1000, ground_material))
 
-    # for a in range(-11, 11):
-    #     for b in range(-11, 11):
-    #         choose_mat = random_double()
-    #         center = Point3(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double())
+    for a in range(-11, 11):
+        for b in range(-11, 11):
+            choose_mat = random_double()
+            center = Point3(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double())
 
-    #         if (center - Point3(4, 0.2, 0)).length > 0.9:
-    #             if choose_mat < 0.8:
-    #                 # diffuse
-    #                 albedo = Color.random() * Color.random()
-    #                 sphere_material = Lambertian(albedo)
-    #                 world.add(Sphere(center, 0.2, sphere_material))
-    #             elif choose_mat < 0.95:
-    #                 # metal
-    #                 albedo = Color.random(0.5, 1)
-    #                 fuzz = random_double(0, 0.5)
-    #                 sphere_material = Metal(albedo, fuzz)
-    #                 world.add(Sphere(center, 0.2, sphere_material))
-    #             else:
-    #                 # glass
-    #                 sphere_material = Dielectric(1.5)
-    #                 world.add(Sphere(center, 0.2, sphere_material))
+            if (center - Point3(4, 0.2, 0)).length > 0.9:
+                if choose_mat < 0.8:
+                    # diffuse
+                    albedo = Color.random() * Color.random()
+                    sphere_material = Lambertian(albedo)
+                    world.add(Sphere(center, 0.2, sphere_material))
+                elif choose_mat < 0.95:
+                    # metal
+                    albedo = Color.random(0.5, 1)
+                    fuzz = random_double(0, 0.5)
+                    sphere_material = Metal(albedo, fuzz)
+                    world.add(Sphere(center, 0.2, sphere_material))
+                else:
+                    # glass
+                    sphere_material = Dielectric(1.5)
+                    world.add(Sphere(center, 0.2, sphere_material))
 
     material_1 = Dielectric(1.5)
     world.add(Sphere(Point3(0.0, 1.0, 0.0), 1.0, material_1))
