b0b7a0d03581cd7256744c31ee13ca5256e982c0
Gökmen Görgen <gkmngrgn@gmail.com>
2020-07-19 15:12:20 +0200

    remove unnecessary function.

--- rayt-cpp/main.cpp
+++ rayt-cpp/main.cpp
@@ -19,19 +19,6 @@
 
 #include <iostream>
 
-double hit_sphere(const point3 &center, double radius, const ray &r) {
-  vec3 oc = r.origin() - center;
-  auto a = r.direction().length_squared();
-  auto half_b = dot(oc, r.direction());
-  auto c = oc.length_squared() - radius * radius;
-  auto discriminant = half_b * half_b - a * c;
-  if (discriminant < 0) {
-    return -1.0;
-  } else {
-    return (-half_b - sqrt(discriminant)) / a;
-  }
-}
-
 color ray_color(const ray &r, const hittable &world, int depth) {
   hit_record rec;
 
