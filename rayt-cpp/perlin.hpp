74cba23851fce8f2474501fc042ad8bbad217ce0
Gökmen Görgen <gkmngrgn@gmail.com>
2020-10-01 04:22:46 +0200

    smoothing perlin spheres.

--- rayt-cpp/perlin.hpp
+++ rayt-cpp/perlin.hpp
@@ -16,6 +16,20 @@
 #include "utils.hpp"
 #include "vec3.hpp"
 
+inline double trilienear_interp(double c[2][2][2], double u, double v,
+                                double w) {
+  auto accum = 0.0;
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 2; j++) {
+      for (int k = 0; k < 2; k++) {
+        accum += (i * u + (1 - i) * (1 - u)) * (j * v + (1 - j) * (1 - v)) *
+                 (k * w + (1 - k) * (1 - w)) * c[i][j][k];
+      }
+    }
+  }
+  return accum;
+}
+
 class perlin {
 public:
   perlin() {
@@ -41,11 +55,22 @@ public:
     auto v = p.y() - floor(p.y());
     auto w = p.z() - floor(p.z());
 
-    auto i = static_cast<int>(4 * p.x()) & 255;
-    auto j = static_cast<int>(4 * p.y()) & 255;
-    auto k = static_cast<int>(4 * p.z()) & 255;
+    int i = floor(p.x());
+    int j = floor(p.y());
+    int k = floor(p.z());
+    double c[2][2][2];
 
-    return ranfloat[perm_x[i] ^ perm_y[j] ^ perm_z[k]];
+    for (int di = 0; di < 2; di++) {
+      for (int dj = 0; dj < 2; dj++) {
+        for (int dk = 0; dk < 2; dk++) {
+          c[di][dj][dk] =
+              ranfloat[perm_x[(i + di) & 255] ^ perm_y[(j + dj) & 255] ^
+                       perm_z[(k + dk) & 255]];
+        }
+      }
+    }
+
+    return trilienear_interp(c, u, v, w);
   }
 
 private:
